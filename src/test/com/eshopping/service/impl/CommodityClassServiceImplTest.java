0a47858c8ae952c1cacaecd64033ef12f0c185f7
281613520 <281613520@qq.com>
2017-03-03 23:16:30 +0800

    测试类，懒得写了。。

---
+++ src/test/com/eshopping/service/impl/CommodityClassServiceImplTest.java
@@ -0,0 +1,31 @@
+package com.eshopping.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by AnKh on 2017/3/3.
+ */
+public class CommodityClassServiceImplTest {
+    @Test
+    public void save() throws Exception {
+
+    }
+
+    @Test
+    public void deleteByCommodityClassId() throws Exception {
+
+    }
+
+    @Test
+    public void updateByCommodityClassId() throws Exception {
+
+    }
+
+    @Test
+    public void findAll() throws Exception {
+
+    }
+
+}
---
+++ src/test/com/eshopping/service/impl/CommodityServiceImplTest.java
@@ -0,0 +1,36 @@
+package com.eshopping.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by AnKh on 2017/3/3.
+ */
+public class CommodityServiceImplTest {
+    @Test
+    public void save() throws Exception {
+
+    }
+
+    @Test
+    public void deleteById() throws Exception {
+
+    }
+
+    @Test
+    public void updateById() throws Exception {
+
+    }
+
+    @Test
+    public void findAll() throws Exception {
+
+    }
+
+    @Test
+    public void findByCommodityId() throws Exception {
+
+    }
+
+}

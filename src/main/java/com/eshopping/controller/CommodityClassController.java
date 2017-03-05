2c51af25006df7a330fe825638a88a2d3ebd5a98
281613520 <281613520@qq.com>
2017-03-05 18:50:57 +0800

    有个借口还没测试 也有点懒得测试了。。

---
+++ src/main/java/com/eshopping/controller/CommodityClassController.java
@@ -0,0 +1,88 @@
+package com.eshopping.controller;
+
+import com.eshopping.entity.CommodityClass;
+import com.eshopping.enums.ExceptionEnums;
+import com.eshopping.service.CommodityClassService;
+import com.eshopping.util.Page;
+import com.eshopping.util.ResultInfo;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by AnKh on 2017/3/5.
+ */
+@Controller
+@RequestMapping(value = "/commodityClass")
+public class CommodityClassController {
+    private org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    CommodityClassService commodityClassService;
+
+    @RequestMapping(value = "/{commodityClassId}",method = RequestMethod.GET)
+    @ResponseBody
+    public ResultInfo findByCommodityClassId(@PathVariable("commodityClassId") Integer commodityClassId){
+        ResultInfo resultInfo;
+        try {
+            CommodityClass commodityClass = commodityClassService.findByCommodityClassId(commodityClassId);
+            resultInfo = new ResultInfo(true,commodityClass);
+            return resultInfo;
+        }catch (RuntimeException e1){
+            return new ResultInfo(false,e1.getMessage());
+        }
+    }
+
+    @RequestMapping(value = "/save" , method = RequestMethod.POST)
+    @ResponseBody
+    public ResultInfo save(@RequestBody CommodityClass commodityClass){
+        ResultInfo resultInfo;
+        try {
+            commodityClassService.save(commodityClass);
+            resultInfo = new ResultInfo(true, ExceptionEnums.SAVE_SUCCESS.getStateInfo());
+            return resultInfo;
+        }catch(RuntimeException e1){
+            return new ResultInfo(false,e1.getMessage());
+        }
+    }
+
+    @RequestMapping(value = "/{commodityClassId}/delete",method = RequestMethod.DELETE)
+    @ResponseBody
+    public ResultInfo deleteByCommodityClassId(@PathVariable("commodityClassId") Integer commodityClassId){
+        ResultInfo resultInfo;
+        try{
+            commodityClassService.deleteByCommodityClassId(commodityClassId);
+            resultInfo = new ResultInfo(true,ExceptionEnums.DELETE_SUCCESS.getStateInfo());
+            return  resultInfo;
+        }catch (RuntimeException e1){
+            return new ResultInfo(false,e1.getMessage());
+        }
+    }
+
+    @RequestMapping(value = "/{commodityClassId}/update",method = RequestMethod.PUT,produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public ResultInfo updateByCommodityClassId(@PathVariable("commodityClassId") Integer commodityClassId,
+                                               @RequestBody CommodityClass commodityClass) {
+        ResultInfo resultInfo;
+        commodityClass.setCommodityClassId(commodityClassId);
+        try{
+            commodityClassService.updateByCommodityClassId(commodityClass);
+            resultInfo = new ResultInfo(true,ExceptionEnums.DELETE_SUCCESS.getStateInfo());
+            return  resultInfo;
+        }catch (RuntimeException e1) {
+            return new ResultInfo(false,e1.getMessage());
+        }
+    }
+
+    @RequestMapping(value = "/list" , method = RequestMethod.POST)
+    @ResponseBody
+    public ResultInfo findByPage(@RequestBody Page page){
+        List<CommodityClass> commodityClassList = commodityClassService.findAll(page.getPageNums(),page.getRows());
+        PageInfo<CommodityClass> pageInfo = new PageInfo<CommodityClass>(commodityClassList);
+        return new ResultInfo(true,commodityClassList,pageInfo.getTotal());
+    }
+
+}
--- src/main/java/com/eshopping/controller/CommodityController.java
+++ src/main/java/com/eshopping/controller/CommodityController.java
@@ -1,13 +1,18 @@
 package com.eshopping.controller;
 
 import com.eshopping.entity.Commodity;
+import com.eshopping.enums.ExceptionEnums;
 import com.eshopping.exception.ResourceNotFoundException;
 import com.eshopping.service.CommodityService;
+import com.eshopping.util.Page;
 import com.eshopping.util.ResultInfo;
+import com.github.pagehelper.PageInfo;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by AnKh on 2017/3/3.
@@ -34,20 +39,53 @@ public class CommodityController {
         return resultInfo;
     }
 
-    @RequestMapping(value = "/{commodityId}",method = RequestMethod.PUT,produces = "application/json;charset=utf-8")
+    @RequestMapping(value = "/{commodityId}/update",method = RequestMethod.PUT,produces = "application/json;charset=utf-8")
     @ResponseBody
     public ResultInfo updateCommodity(@PathVariable("commodityId") Integer commodityId, @RequestBody Commodity commodity){
         ResultInfo resultInfo = null;
         commodity.setCommodityId(commodityId);
         try {
             commodityService.updateById(commodity);
-            resultInfo = new ResultInfo(true,"update successfully");
+            resultInfo = new ResultInfo(true, ExceptionEnums.UPADTE_SUCCESS.getStateInfo());
         }catch (RuntimeException e1){
             return new ResultInfo(false,e1.getMessage());
         }
         return resultInfo;
     }
 
+    @RequestMapping(value = "/{commodityId}/delete",method = RequestMethod.DELETE)
+    @ResponseBody
+    public ResultInfo deleteCommodity(@PathVariable("commodityId") Integer commodityId){
+        ResultInfo resultInfo;
+        try {
+            commodityService.deleteById(commodityId);
+            resultInfo = new ResultInfo(true,ExceptionEnums.DELETE_SUCCESS.getStateInfo());
+        }catch (Exception e){
+            resultInfo = new ResultInfo(false,e.getMessage());
+            return resultInfo;
+        }
+        return resultInfo;
+    }
+
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    @ResponseBody
+    public ResultInfo save(@RequestBody Commodity commodity){
+        try{
+            commodityService.save(commodity);
+            return new ResultInfo(true,ExceptionEnums.SAVE_SUCCESS.getStateInfo());
+        }catch (RuntimeException e1){
+            return new ResultInfo(false,e1.getMessage());
+        }
+    }
+
+    @RequestMapping(value = "/list" ,method = RequestMethod.POST)
+    @ResponseBody
+    public ResultInfo findByPage(@RequestBody Page page){
+        List<Commodity> commodity = commodityService.findAll(page.getPageNums() ,page.getRows());
+        //取记录总条数
+        PageInfo<Commodity> pageInfo = new PageInfo<Commodity>(commodity);
+        return new ResultInfo(true,commodity,pageInfo.getTotal());
+    }
 
 
 }
